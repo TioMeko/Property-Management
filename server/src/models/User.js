6f48fbdc195e18f3f2ffe1780476b66ade3f0dbc
Michael Arasimowicz <61386052+TioMeko@users.noreply.github.com>
2025-11-14 01:07:51 -0500

    updated user model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,16 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true, trim: true },
     passwordHash: { type: String, required: true },
     role: { type: String, enum: ["tenant", "landlord", "admin"], default: "tenant" },
-    name: { type: String, default: "User" }
-    // avatarUrl: { type: String, default: "https://via.placeholder.com/150" },
-    // phone: { type: String, default: "" }, will need validation
-    // currentProperty: { type: mongoose.Schema.Types.ObjectId, ref: "Property" },
-    // avatarUrl: { type: String, default: "https://via.placeholder.com/150" },
-    // subscriptionStatus: { type: String, enum: ["active", "inactive", "past due", "cancelled"], default: "inactive" },
+    name: { type: String, default: "User" },
+    phone: { type: String, default: ""}, // needs validation to make sure it's a number
+    avatarUrl: { type: String, default: "https://via.placeholder.com/150"},
+    currentProperty: {type: mongoose.Schema.Types.ObjectId, ref: "Property" },
+    subscriptionStatus: { type: String, enum: ["active", "inactive", "past due", "cancelled"], default: "inactive" },
   },
   { timestamps: true }
 );
